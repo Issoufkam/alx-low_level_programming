82e8a448c61edbc2d59980fbe06b740e136cf345
Issoufkam <kambireissouf19@gmail.com>
2023-02-17 13:46:01 -0800

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 /**
  * main - A c program that prints a line to the standar error
- * Return : (Success)
+ * Return: (Success)
  */
 int main(void)
 {
-	fput("and that place of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	fputs("and that place of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
 	return (1);
 }
