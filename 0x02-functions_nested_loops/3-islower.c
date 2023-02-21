f301489864a51a1fc36b5990cb14284f6fbf2388
Issoufkam <kambireissouf19@gmail.com>
2023-02-21 13:40:02 -0800

    3-islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,7 +2,7 @@
 
 /**
  * _islower - checks for lowercase character
- * @c The character to be checked
+ * @c: The character to be checked
  * Return: 1 for lowercase character or 0 anything else
  */
 
@@ -12,6 +12,7 @@ int _islower(int c)
 	if (c >= 97 && c <= 122)
 	{
 	return (1);
+
 	}
 	return (0);
 }
