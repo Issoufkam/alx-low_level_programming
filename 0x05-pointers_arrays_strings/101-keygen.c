9e057cd2694915500161af5f4c595f194d07d06e
Issoufkam <kambireissouf19@gmail.com>
2023-02-28 13:04:47 -0800

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -30,11 +30,18 @@ int main(void)
 	diff_half1++;
 	for (index = 0; password[index]; index++)
 	{
-	if (password[index] >= (33 + diff_half2))
+	if (password[index] >= (33 + diff_half1))
 	{
-	password[index] -= diff_half2;
+	password[index] -= diff_half1;
 	break;
 	}
+	}
+	for (index = 0; password[index]; index++)
+	{
+	if (password[index] >= (33 + diff_half1))
+        {
+        password[index] -= diff_half1;
+        break;
 	}
 	}
 	printf("%s", password);
